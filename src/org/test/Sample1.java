5a6b0b41287857bc87487c67efd814a481dc502c
DevikaTesting <k.devi.divi@gmail.com>
2019-03-18 10:55:16 +0530

    new class created

---
+++ src/org/test/Sample1.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Sample1 {
+public static void main(String[] args) {
+	int i=100;
+	if(i%2==0) {
+		System.out.println("even");
+		}else {
+			System.out.println("odd");
+		}
+}
+}
